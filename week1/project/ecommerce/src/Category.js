06cddefd89a9914845393555451b83a6d2e75464
Daniil <daniil.kravchenko.92@yandex.ru>
2023-04-25 22:43:35 +0200

    fix bug with color of selected category

--- week1/project/ecommerce/src/Category.js
+++ week1/project/ecommerce/src/Category.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, {useState} from 'react'
 import categories from "./fake-data/all-categories.js";
 
 export default function Category({selectCategory, categoryId}) {
+  const [selectedCategoryIndex, setSelectedCategoryIndex] = useState(null);
   return (
     <div className='category-container'>
     {categories.map((category, index) => {
       return <div 
       id={index} 
       className={
-        categoryId === index
+        selectedCategoryIndex === index
         ?'category-btn selected'
         :'category-btn'
       }
       onClick={e => {
+        setSelectedCategoryIndex(index)
         selectCategory(e)
       }} 
       key={index}>
